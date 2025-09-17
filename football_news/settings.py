5bc059aeed01caa1080a4eb0c9186414ab2c5dd9
maknoon1a <ahmad.omar@ui.ac.id>
2025-09-17 13:25:56 +0700

    fixed wrong allowed host

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/ahmad.omar/footballnews"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahmad-omar-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
     " https://pbp.cs.ui.ac.id/ahmad.omar/footballnews"
