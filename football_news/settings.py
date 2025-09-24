e711f97d387b0b69bdcbdc3a841ded6caf711ce9
maknoon1a <ahmad.omar@ui.ac.id>
2025-09-24 14:15:02 +0700

    perbaikan csrf trusted url

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahmad-omar-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://ahmad-omar-footballnews.pbp.cs.ui.ac.id/"
+    "https://ahmad-omar-footballnews.pbp.cs.ui.ac.id"
 ]
 
 
